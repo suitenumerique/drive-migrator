6f83c08ff56f69e868e1fc097077d001224ddad8
Nathan Vasse <nathan.vasse@gmail.com>
2024-11-21 17:07:06 +0100

    ✨(back) ignore certain mime types
    Resana automatically triggers errors when uploading html files, so
    to prevent this, we want to directly avoid uploading them to s3.

--- src/backend/core/resana/s3_resana_manager.py
+++ src/backend/core/resana/s3_resana_manager.py
@@ -1,3 +1,4 @@
+import mimetypes
 import os
 
 from django.conf import settings
@@ -24,6 +25,11 @@ class S3ResanaManager:
         for obj in bucket.objects.all():
             logger.info(obj.key)
 
+    def list_bucket_prefix(self, bucket, prefix):
+        logger.info(f"Listing bucket {bucket.name} with prefix {prefix} ...")
+        for obj in bucket.objects.filter(Prefix=prefix):
+            logger.info(obj.key)
+
     def upload_folder(self, workspace: Workspace):
         s3 = boto3.resource(
             "s3",
@@ -38,22 +44,39 @@ class S3ResanaManager:
         if bucket.creation_date is None:
             raise Exception(f"Bucket {bucket_name} does not exist")  # pylint: disable=broad-exception-raised
 
-        # self.list_bucket(bucket)
-
         folder_creator = FolderCreator()
         path = folder_creator.get_workspace_path(workspace)
         destination_path = self.get_destination_path(workspace)
+
+        logger.info(f"Deleting objects with prefix {destination_path}")
+        bucket.objects.filter(Prefix=destination_path).delete()
+        logger.info(f"Listing {destination_path} after deletion ...")
+        self.list_bucket_prefix(bucket, destination_path)
+
+        count = 0
+        excluded_mime_types = ["text/html"]
+
         for root, _dir, files in os.walk(path):
             for name in files:
+                count += 1
                 relative_path = root.replace(path, "")
                 # lstrip is needed because os.path.join will ignore the first argument if it starts with os.sep
                 relative_file_path = os.path.join(
                     destination_path, relative_path.lstrip(os.sep), name
                 )
                 absolute_file_path = os.path.join(root, name)
+
+                mime_type, _ = mimetypes.guess_type(name)
+                if mime_type in excluded_mime_types:
+                    logger.info(
+                        f"({count}) Skipping {absolute_file_path} due to mime type {mime_type}"
+                    )
+                    continue  # Skip files with excluded mime types
+
                 logger.info(
-                    f"Uploading {absolute_file_path} to {relative_file_path} ...."
+                    f"({count}) Uploading {absolute_file_path} to {relative_file_path} ...."
                 )
+
                 bucket.upload_file(absolute_file_path, relative_file_path)
 
         # self.list_bucket(bucket)
