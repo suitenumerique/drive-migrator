3e56b05dbfac56a93e34181c2a7e2a9bb5653799
Lafaye philippe <lafaye@emencia.com>
2026-07-07 09:31:14 +0200

    Update src/backend/core/destinations/drive/backend.py
    Co-authored-by: Nicolas Clerc <720491+kernicPanel@users.noreply.github.com>
    Signed-off-by: Lafaye philippe <lafaye@emencia.com>

--- src/backend/core/destinations/drive/backend.py
+++ src/backend/core/destinations/drive/backend.py
@@ -46,7 +46,7 @@ class DriveDestinationBackend(AbstractDestinationBackend):
 
         self._share_members(backend, workspace, root_id)
 
-        title = _(f"Votre espace {workspace.title} est prêt sur La Suite Drive !")
+        title = _("Votre espace {title} est prêt sur La Suite Drive !").format(title=workspace.title)
         MailsManager().send_migration_mail(
             user, workspace, "drive_ready", {"title": title}
         )
